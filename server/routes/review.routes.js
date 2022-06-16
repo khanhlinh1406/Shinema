09481d7d0ff4fbb7dd34a29e72923b091a72c515
Khanh Linh <72326769+khanhlinh1406@users.noreply.github.com>
2022-06-16 22:36:46 +0700

    add authen token

--- server/routes/review.routes.js
+++ server/routes/review.routes.js
@@ -4,15 +4,15 @@ var router = express.Router()
 const reviewController = require('../controllers/review.controller')
 const { authenToken } = require('../controllers/authen.controller')
 
-router.get('/', reviewController.getAll)
-router.get('/:id', reviewController.getById)
+router.get('/', authenToken, reviewController.getAll)
+router.get('/:id', authenToken, reviewController.getById)
 
 router.post('/', reviewController.create)
 
-router.put('/', reviewController.update)
-router.put('/insertCmt', reviewController.insertCmt)
-router.put('/deleteCmt', reviewController.deleteCmt)
+router.put('/', authenToken, reviewController.update)
+router.put('/insertCmt', authenToken, reviewController.insertCmt)
+router.put('/deleteCmt', authenToken, reviewController.deleteCmt)
 
-router.delete('/:id', reviewController.deleteById)
+router.delete('/:id', authenToken, reviewController.deleteById)
 
 module.exports = router
--- server/routes/ticket.routes.js
+++ server/routes/ticket.routes.js
@@ -2,16 +2,17 @@ const express = require('express')
 var router = express.Router()
 
 const ticketController = require('../controllers/ticket.controller')
+const { authenToken } = require('../controllers/authen.controller')
 
-router.get('/', ticketController.getAll)
-router.get('/:id', ticketController.getById)
-router.get('/user/:email', ticketController.getByUser)
+router.get('/', authenToken, ticketController.getAll)
+router.get('/:id', authenToken, ticketController.getById)
+router.get('/user/:email', authenToken, ticketController.getByUser)
 router.post('/find/bookedSeats/', ticketController.getBookedSeats)
 
 router.post('/', ticketController.create)
 
-router.put('/', ticketController.update)
+router.put('/', authenToken, ticketController.update)
 
-router.delete('/:id', ticketController.deleteById)
+router.delete('/:id', authenToken, ticketController.deleteById)
 
 module.exports = router
